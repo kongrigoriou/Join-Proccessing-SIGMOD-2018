471af6cfef104a897404057e61b508320ce16fcf
Panagiota Flyri <yiota.fliri@gmail.com>
2022-10-15 19:51:29 +0300

    tableFitsCache

--- utilities.c
+++ utilities.c
@@ -1,1 +1,10 @@
 
+#define L2CacheSize 1000; //se MB
+
+int TableFitsCache(int cacheSize, int tableSize){
+    
+    if(tableSize>0 && cacheSize*1024*1024>=tableSize*sizeof(int))
+        return 1;
+    else
+        return 0;
+};
