ba0c2bd996c3fa3b8756207098c63f90ff0bbd0c
dimitrispol <dimitrispol896@gmail.com>
2022-10-16 21:30:03 +0300

    structures debuggin and hopscotch compile

--- hopscotch.c
+++ hopscotch.c
@@ -1,5 +1,14 @@
 #include "structures.h"
 #include <math.h>
+#include <stdlib.h>
+
+
+// node of hopscotch array
+typedef struct node {
+    struct tuple info; //the tuple
+    char  bitmap[64];   //the bitmap
+    
+} node;
 
 int hash2(int key){
     return key;
@@ -9,10 +18,22 @@ int get_hash_i(int key,int size_of_array){
     return hash2(key)%size_of_array;
 }
 
-tuple* create_array(int n){
-    return malloc(n*sizeof(tuple));
+node* create_array(int n){
+    
+    return malloc(n*sizeof(node));
 }
 
-int insert(tuple* array, tuple element, int H){
-    
+int insert(node* array, tuple element, int H){
+    array[0].info=element;
+    array[0].bitmap[0]=1;
+    return 1;
+}
+
+int main(){
+    node* array=create_array(3);
+
+    tuple t;
+    t.key=0;
+    t.payload=0;
+    insert(array,t,2);
 }
--- structures.h
+++ structures.h
@@ -1,9 +1,9 @@
 /** Type definition for a tuple */
-
-struct tuple {
+#include <stdint.h>
+typedef struct tuple {
     int32_t key;
     int32_t payload;
-};
+} tuple;
 /**
 * Type definition for a relation.
 * It consists of an array of tuples and a size of the relation.
