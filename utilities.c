c84a2d237b00d9e74f7a7c3d7deaede5e2f82c1c
Panagiota Flyri <yiota.fliri@gmail.com>
2022-10-18 22:48:51 +0300

    cache offset in KB

--- utilities.c
+++ utilities.c
@@ -1,6 +1,6 @@
-int TableFitsCache(int cacheSize, int tableSize){
+int TableFitsCache(int cacheSize, int tableSize, int offSet){ //offSet se kb
     
-    if(tableSize>0 && cacheSize*1024*1024>=tableSize*sizeof(struct tuple))
+    if(tableSize>0 && ((cacheSize*1024*1024)-(offSet*1024))>=tableSize*sizeof(struct tuple))
         return 1;
     else
         return 0;
