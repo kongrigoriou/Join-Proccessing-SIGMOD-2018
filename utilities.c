a7b25812cdd5755ee4bf03efcba478340499eafa
Panagiota Flyri <yiota.fliri@gmail.com>
2022-10-18 15:52:08 +0300

    updated TableFitsCache

--- utilities.c
+++ utilities.c
@@ -1,7 +1,6 @@
-
 int TableFitsCache(int cacheSize, int tableSize){
     
-    if(tableSize>0 && cacheSize*1024*1024>=tableSize*sizeof(int))
+    if(tableSize>0 && cacheSize*1024*1024>=tableSize*sizeof(struct tuple))
         return 1;
     else
         return 0;
