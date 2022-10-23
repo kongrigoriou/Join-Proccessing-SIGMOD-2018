683c51f8491020b144b4ffbd7163a85b9c5bca80
Panagiota Flyri <yiota.fliri@gmail.com>
2022-10-23 21:55:14 +0300

    fixing errors

--- partition.c
+++ partition.c
@@ -3,41 +3,40 @@
 //  
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
-//h main na kanei malloc ton reOrdered
-/*---
-reOrdered=malloc(sizeof(struct Relation));
-reOrdered->tuples=malloc((endIndex-startIndex+1)*sizeof(struct tuple));
----*/
-void Partition(struct Relation relation, int startIndex, int endIndex, int partitionStep, int numberOfBytes, struct Relation* reOrdered, int* max, int* pSum){
+void Partition(struct relation initialRelation, int startIndex, int endIndex, int partitionStep, int numberOfBytes, struct relation* reOrdered, int* max, int** pSum){
     
-    int hist[pow(2,numberOfBytes)];  //isws na thelei malloc
-    int currentIndex[pow(2,numberOfBytes)]; //boithitikos pinakas pou kratw se poia thesi prepei na topothetisw ton epomeno
+    int* hist;  //isws na thelei malloc
+    int currentIndex[(int)pow(2,numberOfBytes)]; //boithitikos pinakas pou kratw se poia thesi prepei na topothetisw ton epomeno
     int hashedValue;
     
-    pSum = malloc(pow(2,numberOfBytes)*sizeof(int));
-    for(int i=0;i<pow(2,numberOfBytes;i++){
+    *pSum = malloc((int)pow(2,numberOfBytes)*sizeof(int));
+    hist=malloc((int)pow(2,numberOfBytes)*sizeof(int));
+    for(int i=0;i<(int)pow(2,numberOfBytes);i++){
         hist[i]=0;
     }
     max=0;
     for(int j=startIndex;j<=endIndex;j++){
-        hashedValue=relation.tuples[j].key>>((partitionStep-1)*numberOfBytes); // petaw ta teleutaia pshfia pou de xreiazomai
-        hashedValue=hashedValue%(pow(2,numberOfBytes);  //petaw ta prwta pshfia pou de xreiazomai
+        hashedValue=initialRelation.tuples[j].key>>((partitionStep-1)*numberOfBytes); // petaw ta teleutaia pshfia pou de xreiazomai
+        hashedValue=hashedValue%((int)pow(2,numberOfBytes));  //petaw ta prwta pshfia pou de xreiazomai
         hist[hashedValue]++;
-        if(hist[hashedValue]>max)
-            max=hist[hashedValue];
+        if(hist[hashedValue]>*max)
+            *max=hist[hashedValue];
     }
-    pSum[0]=0;
+    (*pSum)[0]=0;
     currentIndex[0]=startIndex;
-    for(int i=1;i<pow(2,numberOfBytes;i++){
-        pSum[i]=pSum[i-1]+hist[i-1];
-        currentIndex[i]+=pSum[i];
+    for(int i=1;i<(int)pow(2,numberOfBytes);i++){
+        (*pSum)[i]=(*pSum)[i-1]+hist[i-1];
+        currentIndex[i]+=(*pSum)[i];
     }
     for(int j=startIndex;j<=endIndex;j++){
-        hashedValue=relation.tuples[j].key>>((partitionStep-1)*numberOfBytes); // petaw ta teleutaia pshfia pou de xreiazomai
-        hashedValue=hashedValue%(pow(2,numberOfBytes);  //petaw ta prwta pshfia pou de xreiazomai
-        reOrdered.tuples[currentIndex[hashedValue]].key=relation.tuples[j].key;
-        reOrdered.tuples[currentIndex[hashedValue]].payload=relation.tuples[j].payload;
+        hashedValue=initialRelation.tuples[j].key>>((partitionStep-1)*numberOfBytes); // petaw ta teleutaia pshfia pou de xreiazomai
+        hashedValue=hashedValue%((int)pow(2,numberOfBytes));  //petaw ta prwta pshfia pou de xreiazomai
+        reOrdered->tuples[currentIndex[hashedValue]].key=initialRelation.tuples[j].key;
+        reOrdered->tuples[currentIndex[hashedValue]].payload=initialRelation.tuples[j].payload;
         currentIndex[hashedValue]++;
     }
+    
 }
