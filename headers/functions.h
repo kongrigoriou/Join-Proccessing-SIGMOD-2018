639df2979d1783325357c4ecd25cfa28ab91cd92
dimitrispol <dimitrispol896@gmail.com>
2022-12-13 22:13:38 +0200

    function and gitignore

--- headers/functions.h
+++ headers/functions.h
@@ -2,7 +2,7 @@
 #define FUNCTIONS
 #include <unistd.h>
 
-#define CACHE_SIZE sysconf(_SC_LEVEL2_CACHE_SIZE)
+#define CACHE_SIZE 256000//sysconf(_SC_LEVEL2_CACHE_SIZE)
 #define HOP_SIZE 1000
 #define N 2
 #define H 10
