c8e3a6e4511cf417893a780cdf2e5427cc2497e6
Konstantina Grigoriou <50745809+kongrigoriou@users.noreply.github.com>
2023-01-20 18:12:49 +0200

    Update functions.h

--- headers/functions.h
+++ headers/functions.h
@@ -1,20 +1,25 @@
 #ifndef FUNCTIONS
 #define FUNCTIONS
 #include <unistd.h>
-#include "jobs.h"
 
 #define CACHE_SIZE 256000//sysconf(_SC_LEVEL2_CACHE_SIZE)
 #define HOP_SIZE 100000
 #define N 2
-#define HN 100
+#define HN  100
+
+#define NUM_OF_THREADS 4
 
 #include "list.h"
 #include "structures.h"
+#include "jobs.h"
+#include "hopscotch.h"
 
 typedef struct relation relation;
 typedef struct result result;
 
-void num_of_partitions(relation*,relation*, int**,relation*,int**,int*,JobList*,int);
-relation PartitionedHashJoin(relation *relR, relation *relS, JobList* jobList, int numberOfThreads);
+int num_of_partitions(relation*,relation*, int**,relation*,int**);
+relation PartitionedHashJoin(relation *relR, relation *relS, JobList*);
+void Probing(hop* hopscotch, relation* reOrdered,int* start, int* finish, List** final);
+
 
 #endif
